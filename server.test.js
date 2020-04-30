ec3b3cd6f0c0553f998b1ffaaa35996a15f6cc6e
AmirHossein Moshfeghi <bullhead1375@gmail.com>
2020-04-30 13:36:45 +0430

    updated server and requests

--- server.test.js
+++ server.test.js
@@ -1,23 +1,10 @@
 const app = require("./server")
 const supertest = require('supertest')
-test("POST /", done => {
-  supertest(app)
-    .post("/api/v1/auth/login/")
-    // api lists:
-    // .post("/api/v1/auth/register/")
-    // .post("/api/v1/auth/verify/")
-    // .post("/api/v1/auth/forgetpassword/")
-    // .post("/api/v1/auth/profile/")
-    // .post("/api/v1/auth/user/")
-    // .post("/api/v1/category/")
-    // .post("/api/v1/profile/ads/")
-    // .post("/api/v1/currency/")
-    // .post("/api/v1/area/")
-    // .post("/api/v1/ads/")
-    // .post("/api/v1/contact/")
-    // .post("/api/v1/admins/ads/")
-
-    .expect(200)
-    
-    .end(done)
-})
+describe('POST /', function () {
+  it('if user is valid ,will be logged in', function (done) {
+      supertest(app)
+          .get("/api/v1/auth/login/")
+          .set('Accept', 'application/json')
+          .expect(200, done);
+  });
+});
