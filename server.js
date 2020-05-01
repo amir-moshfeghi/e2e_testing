d5b2cdbfa00c2ebf38539247c01ad5f9ca105679
AmirHossein Moshfeghi <bullhead1375@gmail.com>
2020-05-01 12:50:41 +0430

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const app = express()
 console.log
 app.post('/', function (req, res, next) {
   let content = req.body;
-  if (content.id) { 
-      return res.status(201).json("user logged in");
+  if (content.username && content.password) { 
+      return res.status(200).json("user logged in");
   }
+  if (content.email && content.password) { 
+    return res.status(201).json("user created");
+}
+if (content.username && content.hash) { 
+  return res.status(200).json("user forgot password");
+}
   return res.status(404).json('user not found');
 });
 
