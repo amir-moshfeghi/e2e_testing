4a882b015579a2a93118ccaf7e54ef2e726e9d3f
AmirHossein Moshfeghi <bullhead1375@gmail.com>
2020-05-01 12:50:10 +0430

    Update server.test.js

--- server.test.js
+++ server.test.js
@@ -7,4 +7,19 @@ describe('POST /', function () {
           .set('Accept', 'application/json')
           .expect(200, done);
   });
+  it('if user is valid ,will be created', function (done) {
+    supertest(app)
+        .get("/api/v1/auth/register/")
+        .set('Accept', 'application/json')
+        .expect(201, done);
+});
+it('if user is valid ,will be create new password', function (done) {
+  supertest(app)
+      .get("/api/v1/auth/forgetpassword/")
+      .set('Accept', 'application/json')
+      .expect(200, done);
+});
+
+
+  
 });
